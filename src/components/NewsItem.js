925b05ec8bd714c2420070b27d8d0f4ce274c752
AbdulMoeez060 <76743473+AbdulMoeez060@users.noreply.github.com>
2021-09-09 21:16:16 +0500

    Added Content in news and newsitem components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+    
+    constructor(){
+        super();
+        console.log("NewsItem Component");
+    }
     render() {
+        let {title, description, imgUrl, newsUrl}=this.props;
+
+
         return (
-            <div>
-                This is a NewsItem
+            <div className="my-3">
+                <div className="card" style={{width: "18rem"}}>
+                    <img src={imgUrl} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/newsDetail" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
             </div>
         )
     }
